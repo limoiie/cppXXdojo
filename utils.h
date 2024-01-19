98aaf4b96225fb949567ccdb333f3ae155b70909
limoiie <limo.iie4@gmail.com>
2024-01-19 17:30:03 +0800

    chore: Group feature testcases by cpp standards

--- utils.h
+++ utils.h
@@ -6,8 +6,8 @@
 
 inline
 void fake_costly_computing(int const seconds = 3) {
-    for (auto i = 0; [[maybe_unused]] auto const _ : std::vector<int>(seconds, 1)) {
-        std::cout << "computing " << i++ << std::endl;
+    for (auto i = 0; i < seconds; ++i) {
+        std::cout << "computing " << i << " seconds" << std::endl;
         std::this_thread::sleep_for(std::chrono::seconds(1));
     }
 }
